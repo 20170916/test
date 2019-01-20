79b152a3eef1349f3ffa903e4e40fbf7cbc5c3cb
20170916 <1334320028@qq.com>
2019-01-20 11:57:51 +0800

    add annotation of ComponentScan to solve NoSuchBeanDefinitionException

--- javase/src/test/java/com/lo/spring/config/AppConfig.java
+++ javase/src/test/java/com/lo/spring/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.lo.spring.config;
 
 import com.lo.spring.pojo.Entitlement;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.lo.spring")
 public class AppConfig {
     @Bean(name="entitlement")
     public Entitlement entitlement() {
