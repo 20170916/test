8ef64310586cce030cdddd663527bc6385934d5d
20170916 <1334320028@qq.com>
2020-02-22 20:22:35 +0800

    [add] hello world about vue

--- test-front/test/src/main.js
+++ test-front/test/src/main.js
@@ -7,9 +7,14 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+/* new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
+}) */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
 })
